d5acf456cf167f9f3569ec37556566cbe6ce6706
alfiexbt <132933913+alfiexbt@users.noreply.github.com>
2023-05-08 13:42:22 -0400

    Add files via upload

---
+++ fuddies landing page/script.js
@@ -0,0 +1,91 @@
+// DROPDOWN MENU
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
+
+
+
+
+
+
+// CAROUSEL 
+const carousel = document.querySelector(".carousel"), 
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i"); 
+
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff; 
+
+
+const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14; 
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    //calling showHideIcons after 60 ms
+    setTimeout(() => showHideIcons(), 60);
+    }); 
+}); 
+
+const autoSlide = () => {
+    if(carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return; 
+    positionDiff = Math.abs(positionDiff); 
+    let firstImgWidth = firstImg.clientWidth + 14; 
+    let valDifference = firstImgWidth - positionDiff; 
+
+    if (carousel.scrollLeft > prevScrollLeft){
+        return carousel.scrollLeft += positionDiff > firstImgWidth/3 ? valDifference : -positionDiff; 
+    }
+    carousel.scrollLeft -= positionDiff > firstImgWidth/3 ? valDifference : -positionDiff;
+     
+}
+
+const dragStart = (e) => {
+    // update globals on mouse down event 
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft; 
+}
+
+const dragging = (e) => {
+    // scrolling images to left according to pointer
+    if (!isDragStart) return; 
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX)  - prevPageX; 
+    carousel.scrollLeft = prevScrollLeft - positionDiff; 
+    showHideIcons(); 
+}
+
+const dragStop = () => {
+    isDragStart = false; 
+    carousel.classList.remove("dragging");
+
+    if (!isDragging) return; 
+    isDragging = false; 
+    autoSlide();
+}
+
+carousel.addEventListener("mousedown",dragStart);
+carousel.addEventListener("touchstart",dragStart);
+
+carousel.addEventListener("mousemove",dragging);
+carousel.addEventListener("touchmove",dragging);
+
+carousel.addEventListener("mouseup",dragStop);
+carousel.addEventListener("mouseleave",dragStop);
+carousel.addEventListener("touchend",dragStop);
